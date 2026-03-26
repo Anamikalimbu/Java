a3a4a07915f6ff7186e6376b1d39c1e872a6b819
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-26 18:03:45 +0545

    arraylist

---
+++ CollegeJava/Array/Demo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Demo
+{
+    public static void main(String [] args){
+        ArrayList list = new ArrayList();
+        list.add("Anamika");
+        list.add(1);
+        list.add(true);
+    }
+}
