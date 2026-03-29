b7f2ec4cf439696db4aff4f00576b08a6ff1b6dd
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-29 16:21:23 +0545

    prime numbers

---
+++ CollegeJava/PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num <= 1) {
+            System.out.println(num + " is not a prime number.");
+        } else {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break; // no need to check further
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num + " is a prime number.");
+            } else {
+                System.out.println(num + " is not a prime number.");
+            }
+        }
+
+        sc.close();
+    }
+}
